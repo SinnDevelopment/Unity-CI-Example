092b7cdf4bd5308683c5e766e64a241eb15d2700
JamieSinn <james.sinn@sinndevelopment.com>
2015-09-19 23:00:08 -0400

    Init Commit

---
+++ Unity CI Example/Assets/Editor/CIBuild.cs
@@ -0,0 +1,21 @@
+﻿﻿using UnityEditor;
+using System;
+using UnityEngine;
+using System.IO;
+
+public class CIBuild
+{
+    public static string[] scenes =
+    {
+            "Assets/Example.unity"
+    };
+    public static void Windows()
+    {
+        var path = Application.dataPath;
+        path = Path.GetDirectoryName(path);
+        path += "/Builds/UnityCIExample.exe";
+        string res = BuildPipeline.BuildPlayer(scenes, path, BuildTarget.StandaloneWindows, BuildOptions.Development);
+        if (res.Length > 0)
+            throw new Exception(res);
+    }
+}
